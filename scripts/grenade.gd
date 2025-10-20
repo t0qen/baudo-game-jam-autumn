52c0c8c0d528892f7695c5b3f0265e22d6d544a1
Als1925 <jules.best.als@gmail.com>
2025-10-20 19:49:41 +0200

    bombe au click

--- scripts/grenade.gd
+++ scripts/grenade.gd
@@ -1,11 +1,15 @@
-extends Area2D
+extends RigidBody2D
 
-@onready var destruction_polygon = $CollisionPolygon2D
+@onready var destruction_polygon = $Area2D/CollisionPolygon2D
 
-func _process(_delta):
-	global_position = get_global_mouse_position()
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		# On récupère le terrain (nom exact: "Terrain1") depuis le parent ou la scène
-		var terrain = get_parent().get_node("Terrain1")
-		if terrain != null:
-			terrain.clip(destruction_polygon)
+func _ready() -> void:
+	$Timer.start()
+	
+
+
+func _on_timer_timeout() -> void:
+	# On récupère le terrain (nom exact: "Terrain1") depuis le parent ou la scène
+	var terrain = get_parent().get_node("Terrain1")
+	if terrain != null:
+		terrain.clip(destruction_polygon)
+	queue_free()
