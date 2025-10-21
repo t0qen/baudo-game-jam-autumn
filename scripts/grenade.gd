48fce7a20b767eed5fe697330e58832313514343
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-21 14:41:49 +0200

    Clean up

--- scripts/grenade.gd
+++ scripts/grenade.gd
@@ -1,11 +1,7 @@
 extends RigidBody2D
 
-@onready var destruction_polygon = $Area2D/CollisionPolygon2D
-
 func _ready() -> void:
 	$Timer.start()
-	
-
 
 func _on_timer_timeout() -> void:
 	# On récupère le terrain (nom exact: "Terrain1") depuis le parent ou la scène
