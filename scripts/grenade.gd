5492bc644afaefce79405229cbc4327959cfdead
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-21 13:00:11 +0200

    Success work of destructible terrain 2d

--- scripts/grenade.gd
+++ scripts/grenade.gd
@@ -9,7 +9,16 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	# On récupère le terrain (nom exact: "Terrain1") depuis le parent ou la scène
-	var terrain = get_parent().get_node("Terrain1")
-	if terrain != null:
-		terrain.clip(destruction_polygon)
+	var terrain = get_parent().get_node("DestructiblePolygon2D")
+	
+	var radius = 100.0
+	var segments = 32
+	var polygon = PackedVector2Array()
+	for i in range(segments):
+		var angle = TAU * i / segments
+		polygon.append(Vector2(cos(angle), sin(angle)) * radius)
+	
+	# "Destruct" prend un polygone (forme du trou) et une position globale
+	var destroyed_area = terrain.destruct(polygon, global_position)
+	print("Destroyed area:", destroyed_area)
 	queue_free()
---
+++ scripts/testcterrain.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var grenade = preload("res://scenes/projectiles/grenade.tscn")
+var grenadeInstance = grenade.instantiate() 
+var mouse_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Faire spawn bombe (pas définitif)"):
+		spawn_enfant()
+
+func spawn_enfant():
+	# Créer une nouvelle instance de la scène
+	var nouvelle_grenade = grenade.instantiate()
+	
+	# Ajouter l'instance comme enfant
+	add_child(nouvelle_grenade)
+	
+	# Positionner l'instance à la position de la souris
+	nouvelle_grenade.global_position = get_global_mouse_position()
