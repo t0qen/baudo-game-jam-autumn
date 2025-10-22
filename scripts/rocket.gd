4b4050a494a73b13e8639da8f188968bdf8d5e5e
Als1925 <jules.best.als@gmail.com>
2025-10-22 10:46:23 +0200

    fesfsfs

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 
 func _on_detection_max_degat_area_entered(area: Area2D) -> void:
 	# On récupère le terrain (nom exact: "Terrain1") depuis le parent ou la scène
-	var terrain = get_parent().get_node("TestMap")
+	var terrain = get_parent().get_node("DestructiblePolygon2D")
 	
 	var radius = 200
 	var segments = 32
