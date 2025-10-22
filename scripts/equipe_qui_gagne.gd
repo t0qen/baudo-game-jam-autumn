596992124367eb7d0a3afcee8e24440f3bdc4f1f
Als1925 <jules.best.als@gmail.com>
2025-10-22 12:56:32 +0200

    zfqzff

--- scripts/equipe_qui_gagne.gd
+++ scripts/equipe_qui_gagne.gd
@@ -4,11 +4,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	VarBidules.is_errant_winner = true
 	if VarBidules.is_errant_winner == true:
 		$Label.text = "Les errants ont gagnés !"
+		$mainerrant.scale.x = -1.2
+		$mainerrant.scale.y = 1.2
+		$errant.scale.x = -1.2
+		$errant.scale.y = 1.2
 	else:
 		$Label.text = "Les gardes ont gagnés !"
+		$maingarde.scale.x = 1.2
+		$maingarde.scale.y = 1.2
+		$garde.scale.x = 1.2
+		$garde.scale.y = 1.2
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
