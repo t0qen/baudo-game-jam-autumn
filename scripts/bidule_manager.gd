4740aa366c153f8fc63ee5463fd703e7c5c81e73
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-23 14:25:29 +0200

    Update bidule_manager.gd

--- scripts/bidule_manager.gd
+++ scripts/bidule_manager.gd
@@ -21,7 +21,7 @@ func set_current(bidule):
 	selected_mob_changed.emit(current_bidule)
 	
 func get_mob_position():
-	return current.global_position
+	return current_bidule.global_position
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
