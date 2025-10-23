bef8d24296a1961c5b7a3b1794bd70f543ebb302
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-10-23 18:22:05 +0200

    dewedwdew

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
-@export var deplacementenpx : float = 25
-@export var zoomenpx : float = 0.01
+@export var deplacement : float = 2
+@export var zoomForce : float = 2
 
 var base_zoom : Vector2 = Vector2(0.2, 0.2)
 var current_zoom : Vector2 
@@ -29,29 +29,36 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("camera_right"):
-		isfocus = false
-		global_position.x += deplacementenpx
-	if Input.is_action_pressed("camera_left"):
-		isfocus = false
-		global_position.x -= deplacementenpx
-	if Input.is_action_pressed("camera_down"):
-		isfocus = false
-		global_position.y += deplacementenpx
-	if Input.is_action_pressed("camera_up"):
-		isfocus = false
-		global_position.y -= deplacementenpx
-	if Input.is_action_pressed("camera_zoom_up"):
-		isfocus = false
-		zoom.x += zoomenpx
-		zoom.y += zoomenpx
-	if Input.is_action_pressed("camera_zoom_down"):
-		isfocus = false
-		zoom.x -= zoomenpx
-		zoom.y -= zoomenpx
-	if Input.is_action_just_pressed("camera_focus"):
-		isfocus = true
-		focus()
+	var movementsVect : Vector2 = Input.get_vector("camera_right", "camera_left", "camera_down", "camera_up")
+	global_position += (movementsVect * deplacement)
+	var zoomVar = Input.get_axis("camera_zoom_down", "camera_zoom_up")
+	zoom += Vector2(zoomVar, zoomVar) * zoomForce
+	if !zoomVar && !movementsVect:
+		global_position = BiduleManager.get_mob_position()
+	
+	#if Input.is_action_pressed("camera_right"):
+		#isfocus = false
+		#global_position.x += deplacementenpx
+	#if Input.is_action_pressed("camera_left"):
+		#isfocus = false
+		#global_position.x -= deplacementenpx
+	#if Input.is_action_pressed("camera_down"):
+		#isfocus = false
+		#global_position.y += deplacementenpx
+	#if Input.is_action_pressed("camera_up"):
+		#isfocus = false
+		#global_position.y -= deplacementenpx
+	#if Input.is_action_pressed("camera_zoom_up"):
+		#isfocus = false
+		#zoom.x += zoomenpx
+		#zoom.y += zoomenpx
+	#if Input.is_action_pressed("camera_zoom_down"):
+		#isfocus = false
+		#zoom.x -= zoomenpx
+		#zoom.y -= zoomenpx
+	#if Input.is_action_just_pressed("camera_focus"):
+		#isfocus = true
+		#focus()
 	
 #
 #func update():
